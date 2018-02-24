4f8cb5d1c746e4f89a5f08193ee5365552144ab7
Paul Schwarzenberger <paul@celidor.net>
2018-02-24 12:01:47 +0000

    deletion of aws-security ec2 instances and elbs

--- destroy-awslab.sh
+++ destroy-awslab.sh
@@ -6,3 +6,6 @@ echo "Deleting user access keys ... "
 custodian run -s. --region=us-east-1 --profile=celidor --cache-period=0 access_key_delete.yml
 echo "User access keys deleted"
 ./delete-mfa.sh
+echo "Deleting environments with the repository tag"
+custodian run -s. --region=eu-west-1 --profile=celidor --cache-period=0 aws_security_delete.yml
+echo "Use the console to delete VPCs and Route53 entries"
