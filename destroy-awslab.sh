131d870dc072dfd48c72c1524436ea9deb1f9c74
Paul Schwarzenberger <paul@celidor.net>
2018-06-11 21:47:29 +0100

    delete serverless training

--- destroy-awslab.sh
+++ destroy-awslab.sh
@@ -8,6 +8,8 @@ echo "User access keys deleted"
 ./delete-mfa.sh
 echo "Deleting Static Web Sites ... "
 python delete-staticwebsite.py --profile celidor
+echo "Deleting Serverless Training ... "
+python delete-serverlesstraining.py --profile celidor
 echo "Deleting environments with the repository tag"
 custodian run -s. --region=eu-west-1 --profile=celidor --cache-period=0 aws_security_delete.yml
 echo "Use the console to delete VPCs and Route53 entries"
